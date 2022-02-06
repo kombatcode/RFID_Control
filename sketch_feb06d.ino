cec7390b32c168258f5b908598f9b01066a8fb8a
Anton Sorokin <kombatgame@yandex.ru>
2022-02-06 23:12:40 +0300

    add file sketch_feb06d.ino

---
+++ sketch_feb06d.ino
@@ -0,0 +1,15 @@
+#define led1 2 // Жёлтый лед
+#define led2 3 // Красный лед
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
