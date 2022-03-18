c114758ae18a385ab67eeaadfe08c6526928949f
kombatcode <86871108+kombatcode@users.noreply.github.com>
2022-03-18 22:07:55 +0300

    Edit
    Edit file

--- sketch_feb06d/sketch_feb06d.ino
+++ sketch_feb06d/sketch_feb06d.ino
@@ -1,5 +1,8 @@
-#define led1 2 // Жёлтый лед
-#define led2 3 // Красный лед
+#include <MFRC522.h>
+MFRC522 rfid(10, 9); // SDA, RST
+
+#define YELLOW_LED 2 // Жёлтый лед
+#define RED_LED 3 // Красный лед
 
 // Сравнение двух массивов известного размера
 bool compareUIDs(uint8_t *in1, uint8_t *in2, uint8_t size) {
@@ -11,14 +14,41 @@ bool compareUIDs(uint8_t *in1, uint8_t *in2, uint8_t size) {
 
 
 void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  Serial.begin(9600);
+  SPI.begin();      // запуск шины
+  rfid.PCD_Init();  // инициализация модуля
+  uint32_t IDsetup;
+    for (byte i = 0; i < 4; i++) {
+      IDsetup <<= 8;
+      IDsetup |= rfid.uid.uidByte[i];
+    }
+    // выведем
+    Serial.println("Setup");    
+    Serial.println(IDsetup, HEX);
+    delay(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  // если поднесена метка
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // запишем метку в 4 байта
+    uint32_t ID;
+    for (byte i = 0; i < 4; i++) {
+      ID <<= 8;
+      ID |= rfid.uid.uidByte[i];
+    }
+    // выведем
+    if (ID != IDsetup) {
+      Serial.println("Error");
+    } else {
+      Serial.println("Success");
+      Serial.println(ID, HEX);
+      delay(500);
+    }
+  }  
 }
