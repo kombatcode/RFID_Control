bf022c384fdf1cc07a66b659719c9ee12c98f527
kombatcode <86871108+kombatcode@users.noreply.github.com>
2022-04-15 17:29:55 +0300

    Update Super-puper-mega-kod.ino

--- Super-puper-mega-kod.ino
+++ Super-puper-mega-kod.ino
@@ -18,17 +18,16 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-  servo.attach(0); // сервомоторчик в исходное положение (закрыто)
+  servo.attach(6); // сервомоторчик в исходное положение (закрыто)
+  servo.write(90);
   pinMode(LED1, OUTPUT);   // 8 пин - выход
   pinMode(LED2, OUTPUT);  // 7 пин - выход
-  pinMode(BTM, INPUT_PULLUP); // 2 пин вход
   // Далее мы показываем, что замок готов к работе, включая на 3 секунды 2 светодиода
   digitalWrite(LED1, HIGH); 
   digitalWrite(LED2, HIGH);
   delay(3000);
   digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  // гасим
+  // гасим зелёный, ради экономии строк, отдельно гасить и включать красный светодиод не будем)
 }
 
 
@@ -49,19 +48,21 @@ void loop() {
     // Если идентификатор не совпадает с заданным номером
     if (uidCard[i] != mfrc522.uid.uidByte[i]) {
       Serial.println("У вас нет доступа");
-      digitalWrite(LED2, HIGH); // Нет доступа, начинаем мигать красным светодиодом
+      digitalWrite(LED2, HIGH); // Нет доступа, мигаем красным светодиодом)
       digitalWrite(LED1, LOW);
-      delay(500);
       digitalWrite(LED2, LOW);
+      delay(500);
       digitalWrite(LED2, HIGH);
+      delay(500);
       return;
     }else{
       Serial.println("Проходите");
       digitalWrite(LED1, HIGH); // Карточка подошла - милости просим, зелёный лед, дверь открыта)
       digitalWrite(LED2, LOW);
-      servo.write(90); // открываем дверь
-      delay(10000); // ждём 10 сек, чтобы человек успел пройти
-      servo.write(0); // и закрываем
+      servo.write(0); // открываем дверь
+      delay(3000); // ждём 10 сек, чтобы человек успел пройти
+      servo.write(90); // и закрываем
+      digitalWrite(LED2, HIGH);
     }
   }
 }
